c3e79188519d2ec1874b17b49e8b8c35bf8b295f
Batu Yekdaneh <93440437+batuykdnh@users.noreply.github.com>
2026-01-02 23:13:35 +0300

    satisfying requirement one

---
+++ hmk4/hmk1/RangePrinter.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class RangePrinter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter two numbers:");
+        int n1 = scanner.nextInt();
+        int n2 = scanner.nextInt();
+        
+        int start = Math.min(n1, n2);
+        int end = Math.max(n1, n2);
+
+        // Prints numbers strictly between inputs
+        for (int i = start + 1; i < end; i++) {
+             System.out.print(i + " ");
+        }
+    }
+}
